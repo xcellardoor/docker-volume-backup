5572ec453f1c30de4d685336b445af36eda91099
xcellardoor <sam@samcater.com>
2017-10-12 11:57:57 +0100

    Better date syntax

--- docker_volume_backup.sh
+++ docker_volume_backup.sh
@@ -11,7 +11,8 @@ set -e
 function backup_volume {
   volume_name=$1
   backup_destination=$2
-  date_suffix=$(date -I)
+  #date_suffix=$(date -I)
+  date_suffix=$(date +%Y-%m-%d-%H:%M)
 
   docker run --rm -v $volume_name:/data -v $backup_destination:/backup alpine:latest tar -zcvpf /backup/$volume_name-$date_suffix.tar /data
 }
